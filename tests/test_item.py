ee63a21eeddddd9e6bc5c0280f83219d226ac5ae
5weeeeet <5weeeeet@mail.ru>
2023-06-27 00:08:31 +0300

    research 2 dz

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,3 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
 """Здесь надо написать тесты с использованием pytest для модуля item."""
@@ -16,3 +15,19 @@ def test_apply_discount():
     Item.pay_rate = 0.8
     test_item1.apply_discount()
     assert test_item1.price == 80.0
+
+
+def test_name():
+    test_item1.name = "Комп"
+    assert test_item1.name == 'Комп'
+    test_item1.name = "СуперКомпьютер"
+    assert test_item1.name == 'СуперКомпь'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('6.0') == 6
+    assert Item.string_to_number('6') == 6
+
+
+def test_instantiate_from_csv():
+    assert len(Item.all) == 2
